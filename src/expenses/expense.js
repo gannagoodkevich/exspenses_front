12e451361a8ddeb8e2732cb6c925e1f2232c1585
ahudkevich <ahudkevich@aristeksystems.com>
2021-06-27 19:56:27 +0300

    feat: add expenses view files

---
+++ src/expenses/expense.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export default function ExpenseCard() {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography className={classes.title} color="textSecondary" gutterBottom>
+            Word of the Day
+          </Typography>
+          <Typography variant="h5" component="h2">
+            be{bull}nev{bull}o{bull}lent
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            adjective
+          </Typography>
+          <Typography variant="body2" component="p">
+            well meaning and kindly.
+            <br />
+            {'"a benevolent smile"'}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+  );
+}
+
+// axios.get('http://localhost:3001/planned_expenses')
+//         .then(response => {
+//             // console.log(response)
+//             this.state = {
+//                 expenses: response.data
+//             }
+//             // console.log(this.state)
+//         })
+        // .catch(error => console.log(error))
